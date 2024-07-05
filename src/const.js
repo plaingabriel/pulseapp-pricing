e0e6c4ef4b5e07b566645e811d6e005cb18dacd0
Gabriel Prado <gaboalespana@gmail.com>
2024-07-05 14:58:43 -0400

    add Cards

---
+++ src/const.js
@@ -0,0 +1,19 @@
+export const pricingCards = [
+  {
+    title: "You get pulse basics, plus:",
+    content: [
+      "Manage cash flow across multiple financial accounts",
+      "Invite your investors, book keeper, or management team to see reports or manage cash flow",
+      "Integrate with QuickBooks Online for more accurate cash flow",
+      "Track your actual cash flow alongside your projected cash flow",
+    ],
+  },
+  {
+    title: "Get it all, plus:",
+    content: [
+      "Manage cash flow across multiple financial accounts",
+      "Convert to any currency for localized cash flow reporting and projections",
+      "Attach invoices or contracts to your cash flow entries for accountability and auditing",
+    ],
+  },
+];
